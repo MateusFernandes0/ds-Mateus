0ec5c9f4ba97ee126603197135a9c8bbee2ad0c4
MateusFernandes0 <162449771+MateusFernandes0@users.noreply.github.com>
2024-03-18 09:18:10 -0300

    Add files via upload

---
+++ aula01/IF.java
@@ -0,0 +1,15 @@
+public class IF {
+    public static void main(String[] args){
+        int x = 20;
+        int y = 20;
+
+        if(x > y){
+            System.out.println("x e maior que y");
+        }else if (x == y){
+            System.out.println("x e y sao iguais");
+        }else{
+            System.out.println("y e maior que x");
+        }
+    }
+    
+}
